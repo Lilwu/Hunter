27a1e0e7abcc8f216a7ed7b0637cd6dfcf897d28
Lilwu <bboywu@gmail.com>
2019-03-13 13:28:45 +0800

    HotkeySlot.cs 第一次提交:20190313

---
+++ HotkeySlot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HotkeySlot : ItemSlot
+{
+    public KeyCode _key;
+
+    public Button _button;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    public override bool CanReceiveItem(Item item)
+    {
+        if (item == null)
+            return true;
+
+        RestorableItem restorableItem = item as RestorableItem;
+        return restorableItem != null;
+    }
+}
