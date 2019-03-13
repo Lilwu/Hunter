aa68dbc2cbc0adf2a66328468491542886da00ae
Lilwu <bboywu@gmail.com>
2019-03-13 13:34:09 +0800

    RestorableItem.cs 第一次提交：20190313

---
+++ RestorableItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RestoreItemType
+{
+    Potion,
+    Scroll,
+}
+
+
+[CreateAssetMenu]
+public class RestorableItem : Item
+{
+    public int add_HP;       //回復血量
+    public int add_MP;       //回復魔力
+    public int add_Speed;    //增加速度
+    [Space]
+    public bool BackToTown;   //傳送回村莊
+
+    public RestoreItemType restoreItemType;
+
+
+    //20190225
+    public bool IsConsumable;
+
+    public virtual void Use(InventoryManager inventoryManager)
+    {
+        inventoryManager.Health += 50;
+        Debug.Log("增加玩家50滴血量");
+    }
+
+    public override Item GetCopy()
+    {
+        return Instantiate(this);
+    }
+}
