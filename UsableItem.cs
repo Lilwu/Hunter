64659912f72a92328d0e537aec1a86ed26bce7e2
Lilwu <bboywu@gmail.com>
2019-03-13 13:37:11 +0800

    UsableItem.cs 第一次提交：20190313

---
+++ UsableItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class UsableItem : Item
+{
+    //20190225
+    public bool IsConsumable;
+
+
+    public virtual void Use(InventoryManager inventoryManager)
+    {
+
+    }
+
+}
