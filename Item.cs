ed78b4cb6cb3374aa956d1e5dfe2eaf9f68b028f
Lilwu <bboywu@gmail.com>
2019-03-13 13:31:40 +0800

    Item.cs 第一次提交：20190313

---
+++ Item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+    [SerializeField] string id;
+    public string ID { get { return id; } }
+    public string ItemName;
+    public Sprite Icon;
+
+    //20190224 
+    [Range(1,999)]
+    public int MaximumStacks = 1;
+
+
+    //20190222 
+    private void OnValidate()
+    {
+        string path = AssetDatabase.GetAssetPath(this);
+        id = AssetDatabase.AssetPathToGUID(path);
+    }
+
+    public virtual Item GetCopy()
+    {
+        return this;
+    }
+
+    public virtual void Destroy()
+    {
+
+    }
+}
+
