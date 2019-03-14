338dd356440f2a7185daf7518db37dbdb97ef194
Lilwu <bboywu@gmail.com>
2019-03-15 01:02:10 +0800

    第一次提交：完成角色追隨，須新增旋轉角度

---
+++ CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float distanceH = 7f;
+    public float distanceV = 4f;
+    public float smoothSpeed = 10f;
+
+    private void LateUpdate()
+    {
+        Vector3 nextpos = Vector3.forward * -1 * distanceH + Vector3.up * distanceV + target.position;
+        this.transform.position = Vector3.Lerp(this.transform.position , nextpos , smoothSpeed * Time.deltaTime);
+        this.transform.LookAt(target);
+    }
+}
