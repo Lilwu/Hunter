150bc1a1a546708728d394627f45330d6e7df509
Lilwu <bboywu@gmail.com>
2019-03-15 01:06:23 +0800

    第一次提交

---
+++ Money.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money : MonoBehaviour
+{
+    public Text amonutText;
+    private int money;
+    private Player player;
+    private PlayerController playerController;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    public void SetMoney(int min, int max)
+    {
+        money = Random.Range(min, max);
+        amonutText.text = "金幣(" + (money.ToString()) + ")";
+    }
+
+    public void AddPlayerMoney()
+    {
+        Debug.Log("你獲得了金幣:" + money);
+        player.AddMoney(money);
+        gameObject.SetActive(false);
+    }
+}
