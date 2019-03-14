209bac06bbc3a44608d932e97b663e9ac56d6b48
Lilwu <bboywu@gmail.com>
2019-03-15 00:56:30 +0800

    第二次提交：合併ItemSetting

--- ItemBase.cs
+++ ItemBase.cs
@@ -1,21 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System;
+﻿using System;
 using UnityEngine;
 
 public class ItemBase : MonoBehaviour
 {
     [SerializeField] Inventory inventory;
+    [SerializeField] InventoryInput inventoryInput;
+    public Item setting;
+    public AudioClip itemClip;
+    private AudioSource audioSource;
 
     public event Action<ItemSlot> OnDropEvent;
 
     private void OnValidate()
     {
         inventory = FindObjectOfType<Inventory>();
+        inventoryInput = FindObjectOfType<InventoryInput>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void OnPickUp()
     {
         Destroy(gameObject);
+    }
+
+    public void PickupItem(ItemBase item)
+    {
+        inventory.AddItem(setting);
     }
 }
