17d6b87ae3b08fda5c04bcc45f882adbf20e91d8
Lilwu <bboywu@gmail.com>
2019-03-19 00:26:00 +0800

    第二次提交：完成攻擊玩家腳本

--- MonsterController.cs
+++ MonsterController.cs
@@ -8,6 +8,7 @@ public class MonsterController : MonoBehaviour
     public Material m_material;
     private Animator _animator;
     private AudioSource _audiosource;
+    public AudioClip attackClip;
 
     //追蹤玩家
     PlayerController isAttack;
@@ -15,10 +16,11 @@ public class MonsterController : MonoBehaviour
     private NavMeshAgent nav;
     private bool playerInRange;
     private bool InAttackRange = false; //玩家可攻擊範圍
+    private float dis;
 
     //攻擊玩家
     private int attackDamage;
-    //private PlayerHealth playerHealth;
+    private PlayerHealth playerHealth;
 
     private float timer;
     private bool playerisDeath = false;
@@ -29,18 +31,27 @@ public class MonsterController : MonoBehaviour
         _animator = GetComponent<Animator>();
         nav = GetComponent<NavMeshAgent>();
         _audiosource = GetComponent<AudioSource>();
+        attackDamage = GetComponent<Monster>().m_attack;
+        playerHealth = FindObjectOfType<PlayerHealth>();
     }
 
     private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.Find("Character").transform;
     }
 
     private void GoToPlayer()
     {
         //追蹤玩家位置
         nav.destination = player.position;
+        _animator.SetBool("IsAttack", false);
         _animator.SetBool("IsRun", true);
+    }
+
+    public void TakeDamageToPlayer() 
+    {
+        _audiosource.PlayOneShot(GetComponent<MonsterHealth>().hitClip , 0.2f);
+        playerHealth.TakeDamge(attackDamage);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -75,9 +86,18 @@ public class MonsterController : MonoBehaviour
 
     private void Update()
     {
-        if (playerInRange)
+        if (dis > 2.5f)
         {
             GoToPlayer();
         }
+        else if(dis <= 2.5f && dis > 1.5f)
+        {
+            nav.destination = player.position;
+            _animator.SetBool("IsRun", false);
+            _animator.SetBool("IsAttack", true);
+        }
+
+        dis = Vector3.Distance(player.position, transform.position);
+        //print(dis);
     }
 }
