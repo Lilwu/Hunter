bac52df7449ddbbd8ff57565ab1e9d889f1937cf
Lilwu <bboywu@gmail.com>
2019-03-19 00:30:32 +0800

    第一次提交：攻擊時彈跳紅色數值腳本，須完成爆擊設定

---
+++ GenerateFont.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateFont : MonoBehaviour
+{
+    GameObject damageCanvas;
+    Text damageText;
+    int criticalHit;
+
+    private void Awake()
+    {
+        damageCanvas = Resources.Load("DamageCanvas", typeof(GameObject)) as GameObject;
+    }
+
+    public void AttackPointFont(int attack)
+    {
+        criticalHit = Random.Range(1, 10);
+        damageText = damageCanvas.transform.Find("DamageText").gameObject.GetComponent<Text>();
+        if (criticalHit == 1)
+        {
+            damageText.color = Color.red;
+            damageCanvas.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+        else
+        {
+            damageText.color = Color.white;
+            damageCanvas.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        }
+       
+        Instantiate(damageCanvas, transform.Find("HeadPoint")).transform.localPosition = transform.position;
+    }
+
+}
