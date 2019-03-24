76464b0ab5f8e5f8281fb917d8e8fe325510b25e
Lilwu <bboywu@gmail.com>
2019-03-25 00:28:40 +0800

    第三次提交：完成體力及魔力回復藥水設定及特效 20190325

--- RestorableItem.cs
+++ RestorableItem.cs
@@ -8,7 +8,6 @@ public enum RestoreItemType
     Scroll,
 }
 
-
 [CreateAssetMenu]
 public class RestorableItem : Item
 {
@@ -20,13 +19,36 @@ public class RestorableItem : Item
 
     public RestoreItemType restoreItemType;
 
-
     //20190225
     public bool IsConsumable;
+    private AudioClip restoreSound;
 
     public virtual void Use(InventoryManager inventoryManager)
     {
-        Debug.Log("增加玩家50滴血量");
+        //恢復體力藥水
+        if(add_HP != 0)
+        {
+            Debug.Log("增加玩家:" + add_HP + "的血量");
+
+            restoreSound = GameObject.Find("Character").GetComponent<PlayerController>().restoreSound;
+            GameObject.Find("Character").GetComponent<AudioSource>().PlayOneShot(restoreSound);
+            GameObject restoreFX_red = Resources.Load("RestoreFX_Red") as GameObject;
+            Instantiate(restoreFX_red).transform.localPosition = GameObject.Find("Character").transform.position;
+
+            FindObjectOfType<PlayerHealth>().AddHp(add_HP);
+        }
+
+        else if(add_MP != 0)
+        {
+            Debug.Log("增加玩家:" + add_MP + "的血量");
+
+            restoreSound = GameObject.Find("Character").GetComponent<PlayerController>().restoreSound;
+            GameObject.Find("Character").GetComponent<AudioSource>().PlayOneShot(restoreSound);
+            GameObject restoreFX_red = Resources.Load("RestoreFX_Blue") as GameObject;
+            Instantiate(restoreFX_red).transform.localPosition = GameObject.Find("Character").transform.position;
+
+            FindObjectOfType<PlayerMagic>().AddMp(add_MP);
+        }
     }
 
     public override Item GetCopy()
