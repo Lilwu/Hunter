486a0c600bbcb5ea8df0de3f4380b8cc26b7c7fb
Lilwu <bboywu@gmail.com>
2019-03-25 00:29:36 +0800

    第一次提交：魔法書攻擊魔法 火焰之舞 20190325

---
+++ MagicalcardItem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum MagicalcardType
+{
+    Attack,
+    Restore
+}
+
+[CreateAssetMenu]
+public class MagicalcardItem : Item
+{
+    public int damage;
+    public int coldtime;
+    public string animation;
+    public string effectName;
+    [Space]
+    public MagicalcardType magicalcardType;
+
+    public virtual void Use(InventoryManager inventoryManager)
+    {
+        Debug.Log("使用魔法書");
+        Player _player = FindObjectOfType<Player>();
+
+        //攻擊型魔法
+        if (magicalcardType == MagicalcardType.Attack)
+        {
+            Debug.Log("使用了:" + ItemName);
+            FindObjectOfType<PlayerController>().UseMagicalCard(animation, effectName);
+
+            Collider[] objs = Physics.OverlapSphere(GameObject.Find("Character").transform.position + Vector3.up, 4.0f, 1 << LayerMask.NameToLayer("Enemy"));
+
+            if(objs.Length <= 0)
+            {
+                return;
+            }
+
+            for (int i = 0; i < objs.Length; i++)
+            {
+                Debug.Log(objs[i].gameObject.name);
+                int r_attack = Random.Range(damage - 15, damage + 30);
+                objs[i].GetComponent<MonsterHealth>().TakeDamge(r_attack);
+                objs[i].GetComponent<GenerateFont>().AttackPointFont(r_attack);
+                FindObjectOfType<DamageFont>().playerAttackPoint = r_attack;
+            }
+        }
+    }
+}
