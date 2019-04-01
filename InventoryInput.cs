32d95fe0fa2818a9f30908c6b69cb36c8fa0fb9e
Lilwu <bboywu@gmail.com>
2019-04-01 20:32:06 +0800

    第四次提交：新增小地圖放大&縮小腳本

--- InventoryInput.cs
+++ InventoryInput.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryInput : MonoBehaviour
 {
     [SerializeField] GameObject equipmentPanelGameObject;
     [SerializeField] GameObject inventoryPanelGameObject;
     [SerializeField] GameObject messagePanelGameObject;
+    [SerializeField] GameObject npcMessagePanelGameObject;
+    [SerializeField] GameObject minimenuPanelGameObject;
+    [SerializeField] GameObject settingPanelGameObject;
     [SerializeField] ItemTooltip itemTooltip;
     [SerializeField] InventoryManager inventoryManager;
+    [SerializeField] Camera minimap;
+    [SerializeField] AudioSource musicAudio;
+    [SerializeField] Slider musicAdjValue;
 
     //20190227 SetHotkey
     [SerializeField] Transform hotkeySlotParent;
@@ -16,23 +23,38 @@ public class InventoryInput : MonoBehaviour
 
     [SerializeField] KeyCode[] toggleEquipmentKeys;
     [SerializeField] KeyCode[] toggleInventoryKeys;
+    [SerializeField] KeyCode[] toggleMinimenuKeys;
+    [SerializeField] KeyCode[] toggleSettingKeys;
+
+    public AudioClip clickClip;
+    private AudioSource audioSource;
 
     private void OnValidate()
     {
         hotkeySlot = hotkeySlotParent.GetComponentsInChildren<HotkeySlot>();
     }
 
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void Start()
     {
         equipmentPanelGameObject.SetActive(false);
         inventoryPanelGameObject.SetActive(false);
+        settingPanelGameObject.SetActive(false);
+        minimap.orthographicSize = 5.0f;
     }
 
     void Update()
     {
+        //背景音樂調整
+        musicAudio.volume = musicAdjValue.value;
+
         for (int i = 0; i < toggleInventoryKeys.Length; i++)
         {
-            if(Input.GetKeyDown(toggleInventoryKeys[i]))
+            if (Input.GetKeyDown(toggleInventoryKeys[i]))
             {
                 itemTooltip.HideTooltip(); //不論開啟關閉物品欄，tooltip始終保持關閉，直到游標指定物品才顯示
                 inventoryPanelGameObject.SetActive(!inventoryPanelGameObject.activeSelf);
@@ -50,6 +72,24 @@ public class InventoryInput : MonoBehaviour
             }
         }
 
+        for (int i = 0; i < toggleMinimenuKeys.Length; i++)
+        {
+            if (Input.GetKeyDown(toggleMinimenuKeys[i]))
+            {
+                minimenuPanelGameObject.SetActive(!minimenuPanelGameObject.activeSelf);
+                break;
+            }
+        }
+
+        for (int i = 0; i < toggleSettingKeys.Length; i++)
+        {
+            if (Input.GetKeyDown(toggleSettingKeys[i]))
+            {
+                settingPanelGameObject.SetActive(!settingPanelGameObject.activeSelf);
+                break;
+            }
+        }
+
         for (int i = 0; i < hotkeySlot.Length; i++)
         {
             if (Input.GetKeyDown(hotkeySlot[i]._key))
@@ -57,10 +97,11 @@ public class InventoryInput : MonoBehaviour
                 hotkeySlot[i]._button.onClick.Invoke();
             }
         }
-    } 
+    }
 
     public void CloseGameObject(GameObject gameObject)
     {
+        CliclSound();
         gameObject.SetActive(false);
     }
 
@@ -72,5 +113,60 @@ public class InventoryInput : MonoBehaviour
     public void CloseMessagePanel()
     {
         messagePanelGameObject.SetActive(false);
+    }
+
+    public void OpenNpcMessagePanel(string text)
+    {
+        npcMessagePanelGameObject.SetActive(true);
+    }
+
+    public void CloseNpcMessagePanel()
+    {
+        npcMessagePanelGameObject.SetActive(false);
+    }
+
+    public void CliclSound()
+    {
+        audioSource.PlayOneShot(clickClip);
+    }
+
+    public void ToogleMinimanuPanel()
+    {
+        CliclSound();
+        minimenuPanelGameObject.SetActive(!minimenuPanelGameObject.activeSelf);
+    }
+
+    public void OpenMinimanu(GameObject panel)
+    {
+        CliclSound();
+        panel.SetActive(!panel.activeSelf);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetQuiality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    //小地圖
+    public void PlusMinimap()
+    {
+        if (minimap.orthographicSize <= 8 && minimap.orthographicSize > 3)
+        {
+            CliclSound();
+            minimap.orthographicSize--;
+        }
+    }
+    public void MinusMinimap()
+    {
+        if (minimap.orthographicSize >= 3 && minimap.orthographicSize < 8)
+        {
+            CliclSound();
+            minimap.orthographicSize++;
+        }
     }
 }
