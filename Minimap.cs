6822512b4bc5f48451bd17a2d15ad1b8d7add911
Lilwu <bboywu@gmail.com>
2019-04-01 20:57:54 +0800

    第一次提交：小地圖系統

---
+++ Minimap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform player;
+    public List<Transform> icons;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+
+        for (int i = 0; i < icons.Count; i++)
+        {
+            icons[i].transform.LookAt(icons[i].transform.position + transform.rotation * Vector3.back,
+                                        transform.rotation * Vector3.up);
+        }
+
+
+    }
+}
