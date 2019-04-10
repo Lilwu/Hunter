e9f41919c7aac183091dcd5c671a74f4b65e83b5
Lilwu <bboywu@gmail.com>
2019-04-11 00:14:36 +0800

    第一次提交：NPC任務界面 20190410

---
+++ MissionPanel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionPanel : MonoBehaviour
+{
+    public Transform missionslotParent;
+    public List<Mission> missions;
+    public GameObject missionPanel;
+
+    public MissionSlot[] missionSlots;
+
+    private void Awake()
+    {
+        ResetMissionPanel();
+        ResetMissionCondition();
+    }
+
+    private void OnValidate()
+    {
+        missionSlots = missionslotParent.GetComponentsInChildren<MissionSlot>();
+    }
+
+    public void ResetMissionPanel()
+    {
+        for (int i = 0; i < missionSlots.Length; i++)
+        {
+            missionSlots[i].mission = missions[i];
+        }
+    }
+
+    public void ResetMissionCondition()
+    {
+        for (int i = 0; i < missionSlots.Length; i++)
+        {
+            missionSlots[i].CheckMissionCondition();
+        }
+    }
+
+    public void ResetOtherMission()
+    {
+        for (int i = 0; i < missionSlots.Length; i++)
+        {
+            missionSlots[i].CheckOtherMission();
+        }
+    }
+
+    public void ClosePanel(GameObject warningPanel)
+    {
+        if(warningPanel.activeSelf == true)
+        {
+            warningPanel.SetActive(false);
+        }
+    }
+}
