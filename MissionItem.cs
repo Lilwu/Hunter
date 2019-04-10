be462599091aae28ec866684cd9d4bd72dab5992
Lilwu <bboywu@gmail.com>
2019-04-11 00:11:35 +0800

    第一次提交：任務道具腳本 20190410

---
+++ MissionItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MissionItemType
+{
+    monsterItem
+}
+
+[CreateAssetMenu]
+public class MissionItem : Item
+{
+    public string itemDescription;
+    public MissionType missionType;
+
+
+    public override Item GetCopy()
+    {
+        return Instantiate(this);
+    }
+}
