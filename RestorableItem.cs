aec03361ef2db9003b193ec77082b939aa630fe7
Lilwu <bboywu@gmail.com>
2019-04-18 11:00:33 +0800

    第五次提交：調整Type命名 20190417

--- RestorableItem.cs
+++ RestorableItem.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum RestoreItemType
 {
-    Potion,
-    Scroll,
-    Stone
+    藥水,
+    卷軸,
+    修復,
 }
 
 [CreateAssetMenu]
@@ -27,11 +29,11 @@ public class RestorableItem : Item
 
     public virtual void Use(InventoryManager inventoryManager)
     {
-        //恢復體力藥水
-        if(add_HP != 0)
-        {
-            Debug.Log("增加玩家:" + add_HP + "的血量");
+        FindObjectOfType<StatePanel>().SetSateText("使用 " + ItemName); //顯示StatePanel
 
+        //恢復體力藥水
+        if (add_HP != 0)
+        {
             restoreSound = GameObject.Find("Character").GetComponent<PlayerController>().restoreSound;
             GameObject.Find("Character").GetComponent<AudioSource>().PlayOneShot(restoreSound);
             GameObject restoreFX_red = Resources.Load("RestoreFX_Red") as GameObject;
@@ -40,10 +42,8 @@ public class RestorableItem : Item
             FindObjectOfType<PlayerHealth>().AddHp(add_HP);
         }
 
-        else if(add_MP != 0)
+        else if (add_MP != 0)
         {
-            Debug.Log("增加玩家:" + add_MP + "的血量");
-
             restoreSound = GameObject.Find("Character").GetComponent<PlayerController>().restoreSound;
             GameObject.Find("Character").GetComponent<AudioSource>().PlayOneShot(restoreSound);
             GameObject restoreFX_red = Resources.Load("RestoreFX_Blue") as GameObject;
@@ -52,15 +52,19 @@ public class RestorableItem : Item
             FindObjectOfType<PlayerMagic>().AddMp(add_MP);
         }
 
-        else if(add_Attack != 0)
+        else if (add_Attack != 0)
         {
-            Debug.Log("增加玩家:" + add_Attack + "的攻擊力");
             restoreSound = GameObject.Find("Character").GetComponent<PlayerController>().restoreSound;
             GameObject.Find("Character").GetComponent<AudioSource>().PlayOneShot(restoreSound);
             GameObject restoreFX_green = Resources.Load("RestoreFX_Green") as GameObject;
             Instantiate(restoreFX_green).transform.localPosition = GameObject.Find("Character").transform.position;
 
             FindObjectOfType<Player>().AddWeaponAttack(add_Attack);
+        }
+
+        else if (BackToTown)
+        {
+            FindObjectOfType<GameLoader>().LoadScene(1);
         }
     }
 
