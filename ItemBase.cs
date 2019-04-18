72b00bc45375c2dc5040111a7ea3bb555ca30923
Lilwu <bboywu@gmail.com>
2019-04-18 10:35:38 +0800

    第三次提交：新增物品HUD追隨攝影機 20190417

--- ItemBase.cs
+++ ItemBase.cs
@@ -11,11 +11,20 @@ public class ItemBase : MonoBehaviour
 
     public event Action<ItemSlot> OnDropEvent;
 
+    //HUD追隨攝影機
+    public GameObject itemHUD;
+    private Camera main_camera;
+
     private void OnValidate()
     {
         inventory = FindObjectOfType<Inventory>();
         inventoryInput = FindObjectOfType<InventoryInput>();
         audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Awake()
+    {
+        main_camera = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
 
     public void OnPickUp()
@@ -26,5 +35,15 @@ public class ItemBase : MonoBehaviour
     public void PickupItem(ItemBase item)
     {
         inventory.AddItem(setting);
+    }
+
+    private void Update()
+    {
+        //HUD追隨攝影機
+        if (itemHUD != null)
+        {
+            itemHUD.transform.LookAt(itemHUD.transform.position + main_camera.transform.rotation * Vector3.back,
+                                        main_camera.transform.rotation * Vector3.up);
+        }
     }
 }
