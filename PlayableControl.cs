1a01c137e6a93c8ddd370b237094fd8fc008cae6
Lilwu <bboywu@gmail.com>
2019-04-27 15:24:53 +0800

    第一次提交：Timeline 設定 20190427

---
+++ PlayableControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PlayableControl : MonoBehaviour
+{
+    public GameObject startCamera;
+    public PlayableDirector pb;
+    private bool isPresent;
+
+    private void Awake()
+    {
+        isPresent = GetComponent<GameManager>().ISPRESENT;
+    }
+
+    private void Start()
+    {
+        if(isPresent)
+        {
+            Destroy(pb.gameObject);
+            Destroy(GetComponent<PlayableControl>());
+            Destroy(startCamera);
+        }
+    }
+
+    private void Update()
+    {
+        if(pb.state == PlayState.Paused && !isPresent )
+        {
+            Destroy(pb.gameObject);
+            Destroy(GetComponent<PlayableControl>());
+            Destroy(startCamera);
+            GetComponent<GameManager>().StartAnnounce();
+        }
+    }
+}
