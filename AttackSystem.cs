b7ac29fe3395130e7d51157444e069b0032834c4
Lilwu <bboywu@gmail.com>
2019-04-27 15:22:55 +0800

    第一次提交：攻擊觸發系統for Weapon 20190427

---
+++ AttackSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSystem : MonoBehaviour
+{
+    public GameObject monsterModel;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Monster")
+        {
+            monsterModel = other.gameObject;
+        }
+        else if (other.tag == "Dragon")
+        {
+            monsterModel = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Monster")
+        {
+            monsterModel = null;
+        }
+        else if (other.tag == "Dragon")
+        {
+            monsterModel = other.gameObject;
+        }
+    }
+}
