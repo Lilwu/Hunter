de041f57743c95630e67f887251396f477b6442b
Lilwu <bboywu@gmail.com>
2019-04-27 15:16:26 +0800

    第五次提交：新增ResetHealth for Levelup 20190427

--- PlayerHealth.cs
+++ PlayerHealth.cs
@@ -8,10 +8,12 @@ public class PlayerHealth : MonoBehaviour
     public Image playerHealth;
     public Text healthText;
     public ParticleSystem hitEffect;
+    public GameObject restartPanel;
     private float maxHealth;
     private int currentHealth;
     private AudioSource _audiosource;
     private Animator _animator;
+    protected bool isDeath;
 
     private void Awake()
     {
@@ -21,12 +23,7 @@ public class PlayerHealth : MonoBehaviour
 
     void Start()
     {
-        maxHealth = GetComponent<Player>().MaxHP;
-        currentHealth = GetComponent<Player>().HP;
-        healthText.text = currentHealth + "/" + maxHealth;
-
-        print("最大血量為：" + maxHealth);
-        print("目前血量為：" + currentHealth);
+        ResetHealth();
     }
 
     public void AddHp(int amount)
@@ -57,26 +54,67 @@ public class PlayerHealth : MonoBehaviour
 
     public void TakeDamge(int damage)
     {
-        _animator.SetBool("GetHit", true);
-        _audiosource.PlayOneShot(GetComponent<PlayerController>().hurtVoiceClip);
-        currentHealth -= damage;
-        playerHealth.fillAmount = currentHealth / maxHealth;
-        healthText.text = currentHealth + "/" + maxHealth;
-
-        FindObjectOfType<StatePanel>().SetSateText("您被扣了" + damage + "滴血"); //顯示StatePanel
-
-        hitEffect.Play();
-
-        if (currentHealth <= 0)
+        if(!isDeath)
         {
-            Death();
+            _animator.SetBool("GetHit", true);
+            _audiosource.PlayOneShot(GetComponent<PlayerController>().hurtVoiceClip);
+            currentHealth -= damage;
+            playerHealth.fillAmount = currentHealth / maxHealth;
+            healthText.text = currentHealth + "/" + maxHealth;
+
+            FindObjectOfType<StatePanel>().SetSateText("您被扣了" + damage + "滴血"); //顯示StatePanel
+
+            hitEffect.Play();
+
+            if (currentHealth <= 0)
+            {
+                Death();
+            }
         }
     }
 
     void Death()
     {
-        healthText.text = 0 + "/" + maxHealth;
+        isDeath = true;
 
+        _animator.SetTrigger("IsDeath");
+        healthText.text = 0 + "/" + maxHealth;
+        GetComponent<PlayerController>().enabled = false;
+        FindObjectOfType<CameraFollow>().enabled = false;
+        _audiosource.PlayOneShot(GetComponent<PlayerController>().deathSoundClip);
         FindObjectOfType<StatePanel>().SetSateText("您已經死亡！"); //顯示StatePanel
+
+        Invoke("OpenRestartPanel", 2.0f);
+    }
+
+    public bool ISDEATH
+    {
+        get { return isDeath; }
+    }
+
+    private void OpenRestartPanel()
+    {
+        restartPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        _animator.ResetTrigger("IsDeath");
+        _animator.SetTrigger("IsLife");
+        GetComponent<PlayerController>().enabled = true;
+        FindObjectOfType<CameraFollow>().enabled = true;
+        AddHp(500);
+        restartPanel.SetActive(false);
+        FindObjectOfType<GameLoader>().LoadScene(1);
+    }
+
+    public void ResetHealth()
+    {
+        maxHealth = GetComponent<Player>().MaxHP;
+        currentHealth = GetComponent<Player>().HP;
+        healthText.text = currentHealth + "/" + maxHealth;
+
+        print("最大血量為：" + maxHealth);
+        print("目前血量為：" + currentHealth);
     }
 }
